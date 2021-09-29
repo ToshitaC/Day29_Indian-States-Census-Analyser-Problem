6610fb50645d2ef0ac31650c2251588e4c733867
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-29 19:13:22 +0530

    Added:Program for to ensure the no of records matches

--- src/main/java/com/censusanalyser/CensusAnalyserTest.java
+++ src/main/java/com/censusanalyser/CensusAnalyserTest.java
@@ -2,16 +2,28 @@ package com.censusanalyser;
 
 import org.junit.Assert;
 import org.junit.Test;
+import com.censusanalyser.CensusAnalyser;
+
+import java.io.IOException;
 
 public class CensusAnalyserTest {
 
     private String INIDAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
     @Test
-        public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
-            CensusAnalyser censusAnalyser = new com.censusanalyser.CensusAnalyser();
-            int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
-            Assert.assertEquals(29,count);
-        }
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+        CensusAnalyser censusAnalyser = new com.censusanalyser.CensusAnalyser();
+        int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29, count);
     }
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29, count);
+    }
+}
+
+
 
