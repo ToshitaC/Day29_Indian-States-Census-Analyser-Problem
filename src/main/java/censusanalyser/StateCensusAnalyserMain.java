b84d7244cd05d5ef6ab2b8ae47c3f75a9e516cf7
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-29 18:36:43 +0530

    Added:Welcome message

---
+++ src/main/java/censusanalyser/StateCensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package censusanalyser;
+
+public class StateCensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("*****WELCOME TO DAY 29 PROGRAM*****");
+    }
+}
