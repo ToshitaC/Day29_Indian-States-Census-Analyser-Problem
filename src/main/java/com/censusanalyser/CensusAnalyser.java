849c2dca00317a714c83b6a4da158970d3a77ad6
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-29 19:53:49 +0530

    Added:Program for to return custom exception

--- src/main/java/com/censusanalyser/CensusAnalyser.java
+++ src/main/java/com/censusanalyser/CensusAnalyser.java
@@ -6,26 +6,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
+import org.junit.rules.ExpectedException;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CensusAnalyser {
-    public int loadIndiaCensusData(String csvFilePath) {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
-            CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
-                    .withType(IndiaCensusCSV.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-            Iterator<IndiaCensusCSV> iterator = csvToBean.iterator();
-
-            Iterable<IndiaCensusCSV> csvIterable = () -> iterator;
-            int count = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
-            return count;
-        } catch (
-                IOException e) {
-            System.out.println(e);
+        public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+            try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+                CsvToBean<IndiaCensusCSV>csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
+                        .withType(IndiaCensusCSV.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+                Iterable<IndiaCensusCSV> csvIterable = () -> iterator;
+                int count = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
+                return count;
+            } catch (IOException e) {
+                throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_INCORRECT);
+            }
+            return 0;
         }
-        return 0;
     }
-}
--- src/main/java/com/censusanalyser/CensusAnalyserTest.java
+++ src/main/java/com/censusanalyser/CensusAnalyserTest.java
@@ -3,25 +3,33 @@ package com.censusanalyser;
 import org.junit.Assert;
 import org.junit.Test;
 import com.censusanalyser.CensusAnalyser;
+import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
 
 public class CensusAnalyserTest {
 
     private String INIDAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private String INIDAN_CENSUS_WrongCSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
-    @Test
-    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
-        CensusAnalyser censusAnalyser = new com.censusanalyser.CensusAnalyser();
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() throws CensusAnalyserException {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
         int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
         Assert.assertEquals(29, count);
     }
 
     @Test
-    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
-        CensusAnalyser censusAnalyser = new CensusAnalyser();
-        int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
-        Assert.assertEquals(29, count);
+    public void givenIndianCensusWrongCSVFile_WhenLoad_ShouldReturnException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_WrongCSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(e.type, CensusAnalyserException.ExceptionType.CENSUS_FILE_INCORRECT);
+            e.printStackTrace();
+        }
+
     }
 }
 
